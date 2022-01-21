434bb998413826d32b6ffe0548935db4f64ea975
AmauryF <amaury.failler@gmail.com>
2022-01-21 12:44:11 +0100

    qrcode transparent

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -21,18 +21,35 @@ class Test extends Component {
   }
   render(){
     const previewStyle = {
-      height: 240,
-      width: 320,
+      position: 'relative',
+      height: '100vh',
+      width: '100%',
+      background : 'black',
     }
 
+    const defineQr = {
+      position: 'absolute',
+      zIndex: 1,
+      display: 'flex',
+      justifyContent: 'center',
+      opacity: 0.5,
+      left : '5%',
+      top: '30%'
+    }
+
+  
+
     return(
-      <div>
+      <div >
         <QrReader
           delay={this.state.delay}
           style={previewStyle}
           onError={this.handleError}
           onScan={this.handleScan}
           />
+          <div style = {defineQr}>
+            <img src = "https://img.freepik.com/free-vector/qr-code-vector-icon-black-qr-code-with-red-frame-isolated-vector-eps-10_532800-402.jpg?size=338&ext=jpg&ga=GA1.2.1696091912.1641772800"></img>
+          </div>
         <p>{this.state.result}</p>
       </div>
     )
